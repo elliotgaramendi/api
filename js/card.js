ab7a79189c0acce4303ebcc465ccb6bcba598b65
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-11 17:25:31 -0500

    js

---
+++ js/card.js
@@ -0,0 +1,33 @@
+import { fetchApi, renderCard } from "../utils/util.js";
+
+const card = async () => {
+  const pokemonLocal = document.getElementById('pokemonLocal');
+  const pokemonRemote = document.getElementById('pokemonRemote');
+  
+  const [localData, remoteData] = await fetchApi('../json/pokeapi-api-v2-pokemon-25.json', 'https://pokeapi.co/api/v2/pokemon/25');
+  
+  renderCard(pokemonLocal, localData);
+  renderCard(pokemonRemote, remoteData);
+
+  // fetch('../json/pokeapi-api-v2-pokemon-25.json')
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     renderCard(pokemonLocal, data);
+  //   });
+
+  // const consultarAPI = async () => {
+  //   const api = await fetch('https://pokeapi.co/api/v2/pokemon/25');
+  //   const data = await api.json();
+  //   renderCard(pokemonRemote, data);
+  // };
+  // consultarAPI();
+
+  // const pokemonPokemonLocal = await fetchApi('../json/pokeapi-api-v2-pokemon-25.json');
+  // const pokemonPokemonRemote = await fetchApi('https://pokeapi.co/api/v2/pokemon/25');
+  // renderCard(pokemonLocal, pokemonPokemonLocal);
+  // renderCard(pokemonRemote, pokemonPokemonRemote);
+}
+
+export default card;
--- js/index.js
+++ js/index.js
@@ -1,48 +1,12 @@
 'use strict';
+import card from './card.js';
+import header from './header.js';
+import loader from './loader.js';
 
-const objetoPersona = {
-  nombre: 'Elliot',
-  meta: 'Ser programador Frontend'
+const documentReady = () => {
+  loader();
+  header();
+  card();
 };
 
-const jsonPersona = '{"nombre":"Elliot","meta":"Ser programador Frontend"}';
-
-// Convertir de objeto a JSON
-console.log(JSON.stringify(objetoPersona));
-// Convertir de JSON a objeto
-console.log(JSON.parse(jsonPersona));
-
-const pokemon1 = document.querySelector('#pokemon1');
-const pokemon2 = document.querySelector('#pokemon2');
-
-const renderPokemon = (name, src, elemento) => {
-  const pokemon = document.createElement('img');
-  pokemon.setAttribute('alt', name);
-  pokemon.setAttribute('src', src);
-  pokemon.classList.add('container__img');
-  elemento.appendChild(pokemon);
-}
-
-// fetch('../json/public-api.json')
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     renderPokemon(data.name, data.src, pokemon1);
-//   });
-
-fetch('https://pokeapi.co/api/v2/pokemon/25')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    renderPokemon(data.name, data.sprites.front_default, pokemon2);
-  });
-
-const consultarAPI = async () => {
-  const api = await fetch('https://elliotxleo.github.io/api-publica/json/public-api.json');
-  const data = await api.json();
-  renderPokemon(data.name, data.src, pokemon1);
-};
-
-consultarAPI();
+document.addEventListener('DOMContentLoaded', documentReady);
